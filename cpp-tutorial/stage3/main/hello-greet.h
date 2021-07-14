8a84c031af0f836373ae98379d434edeae052dd1
paranblues <paran.lee@mail.uk>
2021-07-15 07:45:56 +0900

    Adapt cpplint

--- cpp-tutorial/stage3/main/hello-greet.h
+++ cpp-tutorial/stage3/main/hello-greet.h
@@ -1,8 +1,15 @@
-#ifndef MAIN_HELLO_GREET_H_
-#define MAIN_HELLO_GREET_H_
+/*
+ * Build obj linking with cc_library file example.
+ *
+ * Copyright Bazel organization
+ *
+ */
+
+#ifndef CPP_TUTORIAL_STAGE3_MAIN_HELLO_GREET_H_
+#define CPP_TUTORIAL_STAGE3_MAIN_HELLO_GREET_H_
 
 #include <string>
 
 std::string get_greet(const std::string &thing);
 
-#endif
+#endif  // CPP_TUTORIAL_STAGE3_MAIN_HELLO_GREET_H_
