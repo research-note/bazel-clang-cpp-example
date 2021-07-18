8a3f6932c85d3adec62a269dd655da026bcc1263
paranlee <paran.lee@mail.uk>
2021-07-19 02:36:05 +0900

    Solve linking error in clang

--- cpp-tutorial/stage1/main/hello-world.cc
+++ cpp-tutorial/stage1/main/hello-world.cc
@@ -21,7 +21,7 @@ void print_localtime() {
 int main(int argc, char** argv) {
     std::string who = "world";
     if (argc > 1) {
-      who = argv[1];
+        who = argv[1];
     }
     std::cout << get_greet(who) << std::endl;
     print_localtime();
